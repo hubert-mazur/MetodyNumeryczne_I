3786d36fbfdfbebed3c24ad66fb2bc746a8b24bd
hubert-mazur <hiusvii@gmail.com>
2019-11-21 20:25:35 +0100

    update

--- Lab_03_RRZ_Problemy_Sztywne/Main.cpp
+++ Lab_03_RRZ_Problemy_Sztywne/Main.cpp
@@ -24,18 +24,18 @@ std::pair<double, double> *metoda_trapezow(double xn, double vn, double dT, doub
         F = xn_1 - xn - (dT / 2.0) * (vn + vn_1);
         G = vn_1 - vn - (dT / 2.0) * ((alpha * (1.0 - pow(xn, 2)) * vn - xn) + (alpha * (1.0 - pow(xn_1, 2)) * vn_1 - xn_1));
 
-        a21 = (-dT / 2.0) * (-2 * alpha * xn_1 * vn_1 - 1);
-        a22 = 1.0 - (dT / 2.0) * alpha * (1.0 - pow(xn_1, 2));
+        a21 = (-dT / 2.0) * (-2.0 * alpha * xn_1 * vn_1 - 1.0);
+        a22 = 1.0 - (dT / 2.0) * alpha * (1.0 - pow(xn_1, 2.0));
 
         dX = ((-F) * a22 - (-G) * a12) / (a11 * a22 - a12 * a21);
         dV = (a11 * (-G) - a21 * (-F)) / (a11 * a22 - a12 * a21);
 
         xn_1 += dX;
         vn_1 += dV;
+        if (dX < delta && dV < delta)
+            break;
 
         // std::cout << dX << " and " << dV << "\n";
-        if (dX <= delta && dV <= delta)
-            break;
     }
     return new std::pair<double, double>(xn_1, vn_1);
 }
@@ -43,7 +43,7 @@ std::pair<double, double> *metoda_trapezow(double xn, double vn, double dT, doub
 std::pair<double, double> *metoda_RK2(double xn, double vn, double dT, double alpha)
 {
     double k1x = vn;
-    double k1v = alpha * (1 - pow(xn, 2)) * vn - xn;
+    double k1v = alpha * (1.0 - pow(xn, 2)) * vn - xn;
 
     double k2x = vn + dT * k1v;
     double k2v = alpha * (1.0 - pow(xn + dT * k1x, 2)) * (vn + dT * k1v) - (xn + dT * k1x);
@@ -60,29 +60,35 @@ void timeControl(double x0, double v0, double dT0, double S, int p, double Tmax,
     double Ev;
 
     double TOL[] = {pow(10, -2), pow(10, -5)};
-    FILE *(fileArray)[] = {fopen(&(metoda + std::to_string(TOL[0]) + ".dat")[0], "w"), fopen(&((metoda + std::to_string(TOL[1]) + ".dat"))[0], "w")};
+    FILE *(fileArray)[] = {
+        fopen(&(metoda + std::to_string(TOL[0]) + ".dat")[0], "w"),
+        fopen(&(metoda + std::to_string(TOL[1]) + ".dat")[0], "w")
+        };
+
     std::pair<double, double> *step_n1_2, *step_n_2_2, *step_n_2_1;
 
     for (int j = 0; j < 2; j++)
     {
+        xn = x0;
+        vn = v0;
 
         for (double i = 0.0; i <= Tmax;)
         {
             step_n1_2 = funct(xn, vn, dT, alpha);
             step_n_2_2 = funct(step_n1_2->first, step_n1_2->second, dT, alpha);
 
-            step_n_2_1 = funct(xn, vn, 2 * dT, alpha);
-            Ex = (step_n_2_2->first - step_n_2_1->first) / (pow(2, p) - 1);
-            Ev = (step_n_2_2->second - step_n_2_1->second) / (pow(2, p) - 1);
+            step_n_2_1 = funct(xn, vn, 2.0 * dT, alpha);
+            Ex = (step_n_2_2->first - step_n_2_1->first) / (pow(2, p) - 1.0);
+            Ev = (step_n_2_2->second - step_n_2_1->second) / (pow(2, p) - 1.0);
 
             if (fmax(fabs(Ex), fabs(Ev)) < TOL[j])
             {
-                i = i + 2 * dT;
+                i = i + 2.0 * dT;
                 xn = step_n_2_2->first;
                 vn = step_n_2_2->second;
                 fprintf(fileArray[j], "%lf %lf %lf %lf\n", i, dT, xn, vn);
             }
-            dT = pow((S * TOL[j]) / (fmax(fabs(Ex), fabs(Ev))), 1.0 / (p + 1)) * dT;
+            dT = pow((S * TOL[j]) / (fmax(fabs(Ex), fabs(Ev))), 1.0 / (p + 1.0)) * dT;
             delete step_n1_2;
             delete step_n_2_1;
             delete step_n_2_2;
